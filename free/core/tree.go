1987df4509159bddfc063b892e5021f93ad2eee8
AckermanNan <1178809645@qq.com>
2023-03-27 22:34:57 +0800

    节点搭建

--- free/core/tree.go
+++ free/core/tree.go
@@ -8,7 +8,7 @@ type node struct {
 	isWild   bool    //是否精确匹配:part含有 : || true 时为空
 }
 
-// 匹配节点
+// 第一个匹配节点	插入
 func (n *node) matchChild(part string) *node {
 	for _, child := range n.children {
 		if child.part == part || child.isWild {
@@ -17,3 +17,32 @@ func (n *node) matchChild(part string) *node {
 	}
 	return nil
 }
+
+// 所有匹配成功的节点	查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+// 插入节点
+// func (n *node) insert(pattern string, parts []string, height int) {
+// 	if len(parts) == height {
+// 		n.pattern = pattern
+// 		return
+// 	}
+
+// 	part := parts[height]
+// 	child := n.matchChild(part)
+// 	if child == nil {
+// 		child := &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+// 		n.children = append(n.children, child)
+// 	}
+// }
+
+// 搜索节点
+// func (n *node)

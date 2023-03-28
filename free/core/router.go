8efcbbff888b3a5fe59aab0e81822044c2ea42c8
AckermanNan <1178809645@qq.com>
2023-03-28 22:42:04 +0800

    router搭建

--- free/core/router.go
+++ free/core/router.go
@@ -1,14 +1,50 @@
 package core
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type Router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
-// 添加路由
+func newRouter() *Router {
+	return &Router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// 路由地址切割
+// part != "" 判断时不把最开始 / 符号前面的空内容添加进去
+func (r *Router) parsePattern(pattern string) []string {
+	partsOld := strings.Split(pattern, "/")
+	partsNew := make([]string, 0)
+
+	for _, part := range partsOld {
+		if part != "" {
+			partsNew = append(partsNew, part)
+			if part[0] == '*' {
+				break
+			}
+		}
+	}
+
+	return partsNew
+}
+
+// 添加路由 添加方法分类 ——》 插入树节点 ——》添加对应方法
 func (r *Router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := r.parsePattern(pattern)
+
 	key := pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
 
